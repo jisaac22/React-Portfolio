6a77039a1433d515442b403df0096c101c9dc7c6
jacob sierra <jacobisaac22@yahoo.com>
2021-09-18 19:37:21 -0500

    created slots for projects in project section

--- src/components/pages/aboutMe.js
+++ src/components/pages/aboutMe.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default function AboutMe() {
   return (
-    <section>
+    <section className="aboutMe">
      <div>
        <h2>About Me</h2>
-       <p>HELLO</p>
+       <p>My Name is Jacob Sierra. I'm 29 and a full stack developer in progress. I have been taking a coding course for the past 6 months. 
+         The languages I know are HTML, CSS, Javascript, Node, Express, Mysql and React. I am currently employed with UT Health Science Center
+         in San Antonio but I am working for a career change. In my free time I love to play sports and hang out with family. 
+       </p>
      </div>
     </section> 
   )
--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -2,8 +2,30 @@ import React from "react";
 
 export default function Projects() {
   return (
-    <div>
-      <p>Jacob SIERRA</p>
-    </div>
+    <section>
+      <h2>Projects</h2>
+    
+   <section className="projects">
+     
+     <div className="project1">
+       <p>P1</p>
+     </div>
+     <div className="project1">
+       <p>P2</p>
+     </div>
+     <div className="project1">
+       <p>P3</p>
+     </div>
+     <div className="project1">
+       <p>P4</p>
+     </div>
+     <div className="project1">
+       <p>P5</p>
+     </div>
+     <div className="project1">
+       <p>P6</p>
+     </div>
+   </section>
+   </section>
   )
 }
