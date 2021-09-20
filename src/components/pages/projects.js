d63225113fce51e7ff8e0aa301635cf75545d97d
jacob sierra <jacobisaac22@yahoo.com>
2021-09-20 16:53:05 -0500

    updated css

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -8,28 +8,28 @@ export default function Projects() {
    <section className="projects">
      
      <div className="project1">
-       <a href="https://jisaac22.github.io/Pop-quiz/" >Pop Quiz</a>
-       <a href="https://github.com/jisaac22/Pop-quiz">Github Repo</a>
+       <a href="https://jisaac22.github.io/Pop-quiz/" className="projectName" >Pop Quiz</a>
+       <a href="https://github.com/jisaac22/Pop-quiz"><img src="../icon2.jpg" alt="githubicon" className="icon"></img></a>
      </div>
      <div className="project1">
-       <a href="https://warm-oasis-88552.herokuapp.com/" >Fitness Tracker</a>
-       <a href="https://github.com/jisaac22/Fitness-tracker"><img src="/public/GitHub-logo.png" ></img></a>
+       <a href="https://warm-oasis-88552.herokuapp.com/" className="projectName">Fitness Tracker</a>
+       <a href="https://github.com/jisaac22/Fitness-tracker"><img src="../icon2.jpg" alt="githubicon" className="icon"></img></a>
      </div>
      <div className="project1">
-       <a href="https://mighty-ocean-25788.herokuapp.com/" >Budget Tracker</a>
-       <a href="https://github.com/jisaac22/Budget-tracker">Github Repo</a>
+       <a href="https://mighty-ocean-25788.herokuapp.com/" className="projectName">Budget Tracker</a>
+       <a href="https://github.com/jisaac22/Budget-tracker"><img src="../icon2.jpg" alt="githubicon" className="icon"></img></a>
      </div>
      <div className="project1">
-       <a href="https://mighty-ocean-25788.herokuapp.com/" >Tech Blog</a>
-       <a href="https://github.com/jisaac22/Tech-Blog">Github Repo</a>
+       <a href="https://mighty-ocean-25788.herokuapp.com/" className="projectName" >Tech Blog</a>
+       <a href="https://github.com/jisaac22/Tech-Blog"><img src="../icon2.jpg" alt="githubicon" className="icon"></img></a>
      </div>
      <div className="project1">
-       <a href="https://mrg105.github.io/project-stormhouse/" >Storm House</a>
-       <a href="https://github.com/MrG105/project-stormhouse">Github Repo</a>
+       <a href="https://mrg105.github.io/project-stormhouse/" className="projectName">Storm House</a>
+       <a href="https://github.com/MrG105/project-stormhouse"><img src="../icon2.jpg" alt="githubicon" className="icon"></img></a>
      </div>
      <div className="project1">
-       <a href="https://jisaac22.github.io/Weather-forecast/" >Weather Forecast</a>
-       <a href="https://github.com/jisaac22/Weather-forecast">Github Repo</a> 
+       <a href="https://jisaac22.github.io/Weather-forecast/" className="projectName" >Weather Forecast</a>
+       <a href="https://github.com/jisaac22/Weather-forecast"><img src="../icon2.jpg" alt="githubicon" className="icon"></img></a> 
      </div>
    </section>
    
