b2dda3910e07d7c70957babe6c5628b00f967bbc
jacob sierra <jacobisaac22@yahoo.com>
2021-09-20 21:13:50 -0500

    updated resume information

--- src/components/pages/resume.js
+++ src/components/pages/resume.js
@@ -2,8 +2,32 @@ import React from "react";
 
 export default function Resume() {
   return (
-    <div>
-      <p>Jacob SIERRA</p>
+    <section>
+      <h2>Resume</h2>
+    <div className="proficiencies">
+      <h3>Front-end Proficiencies</h3>
+      <ul className="languages">
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Javascript</li>
+        <li>JQUERY</li>
+        <li>responsive design</li>
+        <li>React</li>
+        <li>Bootstrap</li>
+      </ul>
     </div>
+    <div className="proficiencies">
+      <h3>Back-end Proficiencies</h3>
+      <ul className="languages">
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express</li>
+        <li>Mysql, Sequelize</li>
+        <li>MongoDB, Mongoose</li>
+        <li>Rest</li>
+        <li>GraphQl</li>
+      </ul>
+    </div>
+    </section>
   )
 }
